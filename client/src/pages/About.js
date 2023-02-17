4afba2fa3cbd3cb2d8fe767e9192b3d807cc1835
Chikodi Merenu <cmerenu@gmail.com>
2023-02-17 10:17:26 -0600

    About page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,7 +2,19 @@ const About = () => {
   return (
     <div className="About">
       <h1>About</h1>
-      <h2>This application was used </h2>
+      <h3>Purpose</h3>
+      <h4>
+        This application was used to track of all the items one has in their
+        closet.
+      </h4>
+      <p>
+        As a person add items into their wardrobe, a user can add items into
+        their database, (wardrobe). At any time of day, people can look into
+        what items they have in their closet , this can be very useful as one is
+        shopping and can’t remember if they have a similar item to the one they
+        are considering buying , or to make sure that they can have an outfit
+        that matching something in their closet.
+      </p>
     </div>
   )
 }
